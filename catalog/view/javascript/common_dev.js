677e60bbf5515a3845819dfc14ac397b83a9b8f1
Vladyslav Bykov <bukow.wlad@yandex.ua>
2018-08-11 23:56:46 +0300

    Commit

--- catalog/view/javascript/common_dev.js
+++ catalog/view/javascript/common_dev.js
@@ -40,31 +40,88 @@ $(function() {
 	inputNumber($('.input-number'));
 	/* input counter end */
 
-	//for replaceing path for big img
-	// $(".video_p").click(function() {
-	// 	var src = $(this).find('img').attr('src');
-	// 	console.log(src);
-	// 	$(".main_img").find('img').attr('src', src);
-	// 	console.log(src);
-	// });
-	$(".smol_img").click(function() {
-		var src = $(this).find('img').attr('src');
-		console.log(src);
-		$(".main_img").find('img').attr('src', src);
-		console.log(src);
-	});
-
-		
+		//for replaceing path for big img
+		$(".smol_img").click(function() {
+			var src = $(this).find('img').attr('src');
+			console.log(src);
+			$(".main_img").find('img').attr('src', src);
+			console.log(src);
+		});
 
 	$(".inner_list_img_wrap").slick({
 		// dots: true,
 		infinite: true,
 		slidesToShow: 4,
-		speed: 200,
 		arrows: true,
-		nextArrow: '<div class="right_arrow arrow"><i class="fas fa-chevron-right"></i></div>',
-		prevArrow: '<div class="left_arrow arrow"><i class="fas fa-chevron-left"></i></div>',
-		// slidesToScroll: 3, 
+		prevArrow: '<div class="right_arrow arrow"><i class="fas fa-chevron-right"></i></div>',
+		nextArrow: '<div class="left_arrow arrow"><i class="fas fa-chevron-left"></i></div>',
+		slidesToScroll: 3, 
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: false
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 2,
+					// dots: true,
+					arrows: true
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: true,
+					arrows: false
+				}
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
 	});
 
+	/*Slide down*/
+	// $(".slider-show").click(function (){
+	// 	$(this).closest(".clump").find(".detail").removeClass("closed");
+	// });
+	// $(".slider-close").click(function (){
+	// 	$(this).closest(".clump").find(".detail").addClass("closed");
+	// });
+
+	$(".more").click(function (){
+		$(this).closest(".clump").find(".detail").removeClass("closed");
+		$(this).closest(".clump").find(".more").addClass("hide_link");
+		$(this).closest(".clump").find(".less").removeClass("hide_link");
+	});
+	$(".less").click(function (){
+		$(this).closest(".clump").find(".detail").addClass("closed");
+		$(this).closest(".clump").find(".more").removeClass("hide_link");
+		$(this).closest(".clump").find(".less").addClass("hide_link");
+	});
+
+	$(".header_slide").click(function (){
+		$(this).closest(".clump").find(".detail").removeClass("closed");
+		$(this).closest(".clump").find(".header_slide").addClass("hide_link");
+		$(this).closest(".clump").find(".header_slide_inside").removeClass("hide_link");
+	});
+	$(".header_slide_inside").click(function (){
+		$(this).closest(".clump").find(".detail").addClass("closed");
+		$(this).closest(".clump").find(".header_slide").removeClass("hide_link");
+		$(this).closest(".clump").find(".header_slide_inside").addClass("hide_link");
+	});
+	// $('#nav-icon4').click(function(){
+	// 	$(this).toggleClass('open');
+	// 	$('.mobile-mnu').toggleClass('active');
+	// });
+	
 });
