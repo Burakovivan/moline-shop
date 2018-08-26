eaf7eb40fc82c6429dbf42236df33f7abf242318
Vladyslav Bykov <bukow.wlad@yandex.ua>
2018-08-26 20:46:03 +0300

    Message buy

--- admin/config.php
+++ admin/config.php
@@ -16,11 +16,16 @@ define('HTTPS_SERVER', 'http://localhost/admin/');
 define('HTTPS_CATALOG', 'http://localhost/');
 
 // DIR
-define('DIR_APPLICATION', str_replace('\\','/',__DIR__ ). '/');
-define('DIR_CATALOG', str_replace('\\','/',__DIR__ ). '/../catalog/');
-define('DIR_SYSTEM',  str_replace('\\','/',__DIR__ ). '/../system/');
-define('DIR_IMAGE',  str_replace('\\','/',__DIR__ ). '/../image/');
-define('DIR_STORAGE',  str_replace('\\','/',__DIR__ ). '/../storage/');
+// define('DIR_APPLICATION', str_replace('\\','/',__DIR__ ). '/');
+// define('DIR_CATALOG', str_replace('\\','/',__DIR__ ). '/../catalog/');
+// define('DIR_SYSTEM',  str_replace('\\','/',__DIR__ ). '/../system/');
+// define('DIR_IMAGE',  str_replace('\\','/',__DIR__ ). '/../image/');
+// define('DIR_STORAGE',  str_replace('\\','/',__DIR__ ). '/../storage/');
+define('DIR_APPLICATION', 'J:/xampp/htdocs/admin/');
+define('DIR_SYSTEM', 'J:/xampp/htdocs/system/');
+define('DIR_IMAGE', 'J:/xampp/htdocs/image/');
+define('DIR_STORAGE', 'J:/xampp/htdocs/storage/');
+define('DIR_CATALOG', 'J:/xampp/htdocs/catalog/');
 
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
@@ -33,11 +38,15 @@ define('DIR_SESSION', DIR_STORAGE . 'session/');
 define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
 // DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'sql7.freemysqlhosting.net');
-define('DB_USERNAME', 'sql7253608');
-define('DB_PASSWORD', 'kVxZ13dXgr');
-define('DB_DATABASE', 'sql7253608');
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', 'sql7.freemysqlhosting.net');
+// define('DB_USERNAME', 'sql7253608');
+// define('DB_PASSWORD', 'kVxZ13dXgr');
+// define('DB_DATABASE', 'sql7253608');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'moline_db');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
