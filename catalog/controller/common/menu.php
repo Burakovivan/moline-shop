f62eaf72accc5008a2eced8840e13c511e87a6df
Burakovivan <burakovivan@gmail.com>
2019-01-20 21:55:50 +0200

    Some fixes

--- catalog/controller/common/menu.php
+++ catalog/controller/common/menu.php
@@ -53,6 +53,34 @@ class ControllerCommonMenu extends Controller {
 		} else {
 			$data['logo'] = '';
 		}
+		$data['locations'] = array();
+
+		$this->load->model('localisation/location');
+
+		foreach((array)$this->config->get('config_location') as $location_id) {
+			$location_info = $this->model_localisation_location->getLocation($location_id);
+
+			if ($location_info) {
+				if ($location_info['image']) {
+					$image = $this->model_tool_image->resize($location_info['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_location_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_location_height'));
+				} else {
+					$image = false;
+				}
+
+				$data['locations'][] = array(
+					'location_id' => $location_info['location_id'],
+					'name'        => $location_info['name'],
+					'address'     => nl2br($location_info['address']),
+					'geocode'     => $location_info['geocode'],
+					'telephone'   => $location_info['telephone'],
+					'fax'         => $location_info['fax'],
+					'image'       => $image,
+					'open'        => nl2br($location_info['open']),
+					'comment'     => $location_info['comment']
+				);
+			}
+		}
+		$data['location'] = $data['locations'][count($data['locations'])-1];
 
 		$data['shopping_cart'] = $this->url->link('checkout/cart');
 		
